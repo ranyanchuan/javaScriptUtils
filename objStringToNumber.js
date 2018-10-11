4532986ba896ffbe80d2c1c978a08fc9259155e9
ranyanchuan <ranyanchuan@yimeijian.cn>
2018-10-11 23:52:33 +0800

    uuid

---
+++ objStringToNumber.js
@@ -0,0 +1,30 @@
+/**
+ * value 是字符串的转成Number
+ *
+ */
+export function objStringToNumber(obj) {
+    if (Array.isArray(obj)) {
+        obj.map((childItem, index) => {
+            if (typeof childItem === 'object') {
+                objStringToNumber(childItem);
+            } else {
+                if ((typeof childItem !== 'number')) {
+                    const temp = Number(childItem);
+                    if (temp) obj[index] = temp;
+                }
+            }
+        })
+    } else {
+        for (const item in obj) {
+            if (typeof obj[item] === 'object') {
+                objStringToNumber(obj[item]);
+            } else {
+                if (obj[item] && (typeof obj[item] !== 'number')) {
+                    const temp = Number(obj[item]);
+                    if (temp) obj[item] = temp;
+                }
+            }
+        }
+    }
+    return obj;
+}
