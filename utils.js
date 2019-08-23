06be0a6c6f3fd16dfc9f0605620582a7c2f58470
ranyanchuan <ranyanchuan@yimeijian.cn>
2019-08-23 20:14:15 +0800

    浏览器全屏

--- utils.js
+++ utils.js
@@ -194,3 +194,41 @@ export function objDctValue(data, childrenKey) {
     return result;
 }
 
+
+
+/**
+ *
+ * 浏览器全屏
+ */
+export function fullScreen() {
+    let el = document.documentElement;
+    let rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen;
+    if(typeof rfs != "undefined" && rfs) {
+        rfs.call(el);
+    };
+    return;
+}
+
+/**
+ *
+ * 退出全屏
+ */
+export function exitScreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    }
+    else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    }
+    else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen();
+    }
+    else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+    if(typeof cfs != "undefined" && cfs) {
+        cfs.call(el);
+    }
+}
+
+
