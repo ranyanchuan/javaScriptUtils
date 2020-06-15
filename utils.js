3d91c2df4e348d79efcf827c80995a11f9b75611
ranyanchuan <ranyanchuan@yimeijian.cn>
2020-06-15 10:23:13 +0800

    字符串统计

--- utils.js
+++ utils.js
@@ -1273,8 +1273,28 @@ export const isPrime = (input) => {
 
 /**
  *
- * 邮箱
+ * 判断字符串出现最多的字符，并统计次数
  */
+export const countStr = (str) => {
+    var obj = {};
+    for (var i = 0, l = str.length, k; i < l; i++) {
+        k = str.charAt(i);
+        if (obj[k]) {
+            obj[k]++;
+        } else {
+            obj[k] = 1;
+        }
+    }
+    var m = 0, i = null;
+    for (var k in obj) {
+        if (obj[k] > m) {
+            m = obj[k];
+            i = k;
+        }
+    }
+    return i + ':' + m;
+}
+
 
 /**
  *
